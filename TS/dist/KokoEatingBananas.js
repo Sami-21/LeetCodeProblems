114c9f7d3ba5b746fc53ca1d19408d72e07ecc61
Sami-21 <samovitch360@gmail.com>
2022-01-20 20:39:15 +0100

    starting the daily challange

---
+++ TS/dist/KokoEatingBananas.js
@@ -0,0 +1,9 @@
+//----------------------------Step 1----------------------------//
+// Our friend koko wants to eat all the piles of Bananas before the guard finds out , he wants a minimum eating speed to eat all the piles.
+// Small note : when he eats a pile he always rest until the next hour before starting to eat again .   
+//----------------------------Step 2----------------------------//
+// input : piles arrray of positive integers , h hours before the guards come back.
+//----------------------------Step 3----------------------------//
+// We will try to give the optimal solution by taking into consideration readability , time and space complexity. 
+//----------------------------Step 4----------------------------//
+// DON'T BE AN ANNOYING FUCKTARD 
