9306e8ef7e87be81db696dc57def57b19d78ec16
Sami-21 <samovitch360@gmail.com>
2022-02-16 11:03:43 +0100

    daily challange of yesterday completed

---
+++ TS/FindtheDifference.ts
@@ -0,0 +1,47 @@
+//----------------------------Step 1----------------------------//
+
+// We are given a string "s" , by randomly shuffling this string's another string "t" is generated then a new character is added at a random position .The added string needs to be returned
+
+//----------------------------Step 2----------------------------//
+
+// Input : Two Strings "S" and "T"
+// Output : character (the difference between the two strings)
+
+//----------------------------Step 3----------------------------//
+
+// We assume that our main goal is to give the optimal solution ( taking into consideration time complexity , space complexity and readability ) .
+
+//----------------------------Step 4----------------------------//
+
+// Don't be an annoying F*ckwit
+
+//----------------------------Step 5----------------------------//
+
+// Brute Force / Naive Approach 
+// We simply loop through the second string (T) and for each character we loop through the first string (S) looking for a match .
+// If we find a match we simply jump to the next character in the T string
+// If no match is found we return the character .
+
+//----------------------------Step 6----------------------------//
+
+// This approach is not the optimal solution because we are using two nested loops which means that our time complexity is O(n²) 
+
+//----------------------------Step 7----------------------------//
+
+// Alternative Approach 
+// We can use hash tables
+
+
+let S:string= "dafuq",T:string="dafuqa";
+S = S.replace=("daf","fuck");
+const findTheDifference = (s: string, t: string): string => {
+ 
+    for(let i:number=0;i<t.length;i++){
+        let exists=false;
+        for(let j:number=0;j<s.length;j++){
+        }
+    }
+  
+    };
+    console.log(findTheDifference(S,T))
+
---
+++ TS/dist/SingleNumber.js
@@ -0,0 +1,35 @@
+//----------------------------Step 1----------------------------//
+// We are given a non-empty array of integers , each element of the array appears twice only one appears one time , we return that element.
+//----------------------------Step 2----------------------------//
+// Input : array of integers
+// Output : Integer
+//----------------------------Step 3----------------------------//
+// Our main goal is to solve it with time complexity of O(n) and space complexity O(1)
+//----------------------------Step 4----------------------------//
+// Don't be an annoying F*ckwit
+//----------------------------Step 5----------------------------//
+// Brute force / naive approach
+// We can use two nested loops to traverse the array and check if the element appears twice if not we simply return the element. 
+//----------------------------Step 6----------------------------//
+// This approach have a Time Complexity O(n²) and our main goal is O(n) 
+//----------------------------Step 7----------------------------//
+// Alternative Approach (Using XOR)
+// Quick reminder : a XOR a = 0
+//                  a XOR 0 = 0
+//                  XOR is associative ==> a XOR (b XOR (c XOR d)) = a XOR b XOR c XOR d
+// XOR operator compare bits, example 100 XOR 001 = 101 (4 XOR 1 = 5) 
+//----------------------------Step 8----------------------------//
+// We loop through the array and  we use the XOR operator , since all elements appears twice except one , all pairs will equal 0 (a XOR a = 0)
+// We also have XOR is associative and  the only element will be left along with zero's from all pairs (a XOR 0 = a)
+// We return the result  
+//----------------------------Step 9----------------------------//
+var Nums = [1, 2, 5, 7, 8, 5, 3, 14, 8, 7, 1, 2, 14];
+var singleNumber = function (nums) {
+    // where the result is saved
+    var result = nums[0];
+    // array traversal
+    for (var i = 1; i < nums.length; i++) {
+        result ^= nums[i]; // result = result ^ nums[i] (^ is the XOR operator in JS + TS)
+    }
+    return result;
+};
