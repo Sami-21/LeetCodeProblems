e78552e580f6bbcf11f1bcf07e9b9f3fd0a9375d
Sami-21 <samovitch360@gmail.com>
2022-03-02 18:06:28 +0100

    I forgot what i did

--- TS/ExcelSheetColumnNumber.ts
+++ TS/ExcelSheetColumnNumber.ts
@@ -1,24 +1,19 @@
-//----------------------------Step 1----------------------------//
+// Problem Overview
 
 // We are given the number of a row in excel sheet which is represented with a String ,
 // We return the corresponding number in decimal   
 
-//----------------------------Step 2----------------------------//
+// Expected Input / Output
 
 // Input : String ( Column ).
 // Output : Integer ( corresponding value in decimal )
 
-//----------------------------Step 3----------------------------//
+//Main Goal
 
-// We assume our aim to give the best solution possible ( Optimal balance between time and space complexity and the best readability ) 
-
-//----------------------------Step 4----------------------------//
-
-// HADOOOOOOOOOOOOOOOOUKEN
-
-//----------------------------Step 5----------------------------//
+// We assume our aim to give the best solution possible ( Optimal balance between time , space complexity and readability ) 
 
 // Brute Force / Naive Approach 
+
 // we follow the same path when we convert from binary or hexadecimal to decimal . 
 // Quick reminder (10111)² => 1*2^4 + 0*2^3 + 1*2^2 + 1*2^1 + 1*2^0 
 // We initiate an output variable where we store the result ( initial value zero )
@@ -28,21 +23,21 @@
 // instead of using 2 or 16 our new system uses 26 as it base   (from A = 1 , B = 2 ..... to Z = 26 , total 26 )
 // small example : 'ABA' => A*26^2 + B*26^1 + A*26^0 where A = 1 and B = 2 
 // result 729
+// Our time complexity is O(n) , space Complexity O(1)
+// Our readability however is pretty bad.
 
-//----------------------------Step 6----------------------------//
+// Previous Approach drawback
 
 // This approach affects readability since our code will have a large switch statement
 
-//----------------------------Step 7 + 8----------------------------//
-
 // Alternative appraoch ( using ASCII code )
-// instead of using the switch statement 
+
+// instead of using the switch statement , We calculate the value of each character using it's ASCII code.
 // We know that "A" ASCII code is 65 and A represents 1 which implements whatever character we have X it's represented value in decimal is equal to ( (X)ASCII code - 64 )   
 // element = ( el ASCII - 64 ) * 26 ^ ( string size - el index - 1 )
 // element value is appended to the output .
-// After we finish traversing the whole string we should have the corresponding decimal value
+// After we finish traversing the whole string we should have the corresponding decimal value.
   
-//----------------------------Step 9 + 10----------------------------//
 
 const titleToNumber = ( columnTitle:string ):number => {
 
--- TS/dist/ExcelSheetColumnNumber.js
+++ TS/dist/ExcelSheetColumnNumber.js
@@ -1,14 +1,11 @@
-//----------------------------Step 1----------------------------//
+// Problem Overview
 // We are given the number of a row in excel sheet which is represented with a String ,
 // We return the corresponding number in decimal   
-//----------------------------Step 2----------------------------//
+// Expected Input / Output
 // Input : String ( Column ).
 // Output : Integer ( corresponding value in decimal )
-//----------------------------Step 3----------------------------//
-// We assume our aim to give the best solution possible ( Optimal balance between time and space complexity and the best readability ) 
-//----------------------------Step 4----------------------------//
-// HADOOOOOOOOOOOOOOOOUKEN
-//----------------------------Step 5----------------------------//
+//Main Goal
+// We assume our aim to give the best solution possible ( Optimal balance between time , space complexity and readability ) 
 // Brute Force / Naive Approach 
 // we follow the same path when we convert from binary or hexadecimal to decimal . 
 // Quick reminder (10111)² => 1*2^4 + 0*2^3 + 1*2^2 + 1*2^1 + 1*2^0 
@@ -19,16 +16,16 @@
 // instead of using 2 or 16 our new system uses 26 as it base   (from A = 1 , B = 2 ..... to Z = 26 , total 26 )
 // small example : 'ABA' => A*26^2 + B*26^1 + A*26^0 where A = 1 and B = 2 
 // result 729
-//----------------------------Step 6----------------------------//
+// Our time complexity is O(n) , space Complexity O(1)
+// Our readability however is pretty bad.
+// Previous Approach drawback
 // This approach affects readability since our code will have a large switch statement
-//----------------------------Step 7 + 8----------------------------//
 // Alternative appraoch ( using ASCII code )
-// instead of using the switch statement 
+// instead of using the switch statement , We calculate the value of each character using it's ASCII code.
 // We know that "A" ASCII code is 65 and A represents 1 which implements whatever character we have X it's represented value in decimal is equal to ( (X)ASCII code - 64 )   
 // element = ( el ASCII - 64 ) * 26 ^ ( string size - el index - 1 )
 // element value is appended to the output .
-// After we finish traversing the whole string we should have the corresponding decimal value
-//----------------------------Step 9 + 10----------------------------//
+// After we finish traversing the whole string we should have the corresponding decimal value.
 var titleToNumber = function (columnTitle) {
     // Output variable initialized
     var output = 0;
