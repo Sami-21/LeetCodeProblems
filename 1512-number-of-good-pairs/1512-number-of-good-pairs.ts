9a4a81bcd0c6f930d569a4f56ec2ef4f1e1ad5c8
Sami Maachi <55092275+Sami-21@users.noreply.github.com>
2022-05-29 22:31:49 +0100

    Time: 115 ms (6.93%), Space: 43 MB (72.28%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.ts
@@ -0,0 +1,9 @@
+function numIdenticalPairs(nums: number[]): number {
+    let output:number=0;
+    for(let i:number=0;i<nums.length-1;i++){
+      for(let j:number=i+1;j<nums.length;j++){
+        if(nums[i] == nums[j]) output++;
+    }   
+    }
+    return output;
+};
