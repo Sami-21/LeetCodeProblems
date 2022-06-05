60c04d21584c7e7c05617d1ddbc53dfc615db70a
Sami Maachi <55092275+Sami-21@users.noreply.github.com>
2022-06-05 20:32:55 +0100

    Time: 125 ms (20.00%), Space: 43.3 MB (33.33%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.ts
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.ts
@@ -1,12 +1,9 @@
 function xorOperation(n: number, start: number): number {
-    let start1:number = start;
     let output:number[] = [];
     for(let i:number=0;i<n;i++){
-        output.push(start1);
-        start1+=2;
+        output.push(start);
+        start+=2;
     }
-    console.log(3^5)
     return output.reduce((total , num ) => {
-        console.log(total ^ num);
         return total ^ num} )
 };
