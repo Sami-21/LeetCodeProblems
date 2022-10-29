03e5c2d3f357f50a5baa2d6801f265c04e7a8ab3
Sami Maachi <55092275+Sami-21@users.noreply.github.com>
2022-10-29 13:00:11 +0200

    Time: 81 ms (88.70%), Space: 46.4 MB (12.35%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.ts
@@ -0,0 +1,16 @@
+function isHappy(n: number): boolean {
+    let previousResult:any = {};
+    let sum:number = n;
+    console.log(sum);
+    while(sum!==1){
+        sum = sum.toString().split("").map((el) => {
+        return parseInt(el);
+    }).reduce((total,num) => {
+        return total + num**2;
+    },0) ;
+        if(previousResult[sum]) return false;
+        else    previousResult[sum]=true;
+    } 
+   
+    return true 
+};
