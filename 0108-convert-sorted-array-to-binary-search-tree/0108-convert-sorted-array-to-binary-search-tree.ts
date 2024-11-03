9a7138f7eeac71eeb7a193709ea77d6c3621ca75
Sami Maachi <55092275+Sami-21@users.noreply.github.com>
2024-11-03 19:13:46 +0100

    Time: 1 ms (87.69%), Space: 54.1 MB (36.55%) - LeetHub

---
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.ts
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function sortedArrayToBST(nums: number[]): TreeNode | null {
+  if (nums.length === 0) return null;
+  if (nums.length === 1) return new TreeNode(nums[0]);
+  const center = Math.floor(nums.length / 2);
+  return new TreeNode(
+    nums[center],
+    sortedArrayToBST(nums.slice(0, center)),
+    sortedArrayToBST(nums.slice(center + 1))
+  );
+}
