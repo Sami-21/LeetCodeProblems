7da3fd27af633689fdafd82f13f6822fcc050e9b
Sami Maachi <55092275+Sami-21@users.noreply.github.com>
2026-08-26 08:54:34 +0100

    Time: 0 ms (100%), Space: 4.1 MB (85.5%) - LeetHub

--- 0067-add-binary/0067-add-binary.go
+++ 0067-add-binary/0067-add-binary.go
@@ -1,27 +1,31 @@
-import "slices"
-
 func addBinary(a string, b string) string {
-    result := []byte{};
+    result := make([]byte,max(len(a),len(b))+1);
+    resultIterator := len(result)-1
+
     iteratorA := len(a) - 1;   
     iteratorB := len(b) - 1 ;
-    reminder := byte(0);
-    for iteratorA >= 0 || iteratorB >= 0 || reminder > 0 {
-        aChar := byte('0');
-        if iteratorA >= 0 {
-            aChar = a[iteratorA]; 
-        }
-        bChar := byte('0');
-        if iteratorB >= 0 {
-            bChar = b[iteratorB]; 
-        }
-        sum := (aChar - '0') + (bChar - '0') + reminder;
+    carry := byte(0);
 
-        reminder = sum / 2;
-        result = append(result,(sum % 2)+'0');
+    for iteratorA >= 0 || iteratorB >= 0 || carry > 0 {
+        aChar := byte(0);
+        if iteratorA >= 0 {
+            aChar = a[iteratorA]  - '0'; 
+        }
+
+        bChar := byte(0);
+        if iteratorB >= 0 {
+            bChar = b[iteratorB] - '0'; 
+        }
+
+        sum := aChar + bChar + carry;
+
+        carry = sum / 2;
+        result[resultIterator] = (sum % 2)+'0';
+
         iteratorA--;
         iteratorB--;
+        resultIterator--;
     }    
-    slices.Reverse(result)
 
-    return string(result);
+    return string(result[resultIterator+1:])
 }
